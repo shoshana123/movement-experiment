048006e6c5aa850f9601af57b5e2cceacd4f9664
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-08 13:08:05 -0400

    updated to include server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+let app = express();
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static('dist'));
+} else {
+    app.use(express.static('dist'));
+}
+
+app.get('/', (req, res)=>{
+    if (process.env.NODE_ENV !== 'production') {
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
+    } else {
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log('Started');
+});
