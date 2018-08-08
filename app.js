98f294fd64412eb6fc3e86cfeaf43b0ec3afb527
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-08 14:13:49 -0400

    updated app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const path = require('path');
 const PORT = process.env.PORT || 3002;
 let app = express();
 if (process.env.NODE_ENV !== 'production') {
-    app.use(express.static('/'));
+    app.use(express.static('dist'));
 } else {
-    app.use(express.static('/'));
+    app.use(express.static('dist'));
 }
 
 app.get('/', (req, res)=>{
     if (process.env.NODE_ENV !== 'production') {
-        res.sendFile(path.join(__dirname+'/index.html'));
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
     } else {
-        res.sendFile(path.join(__dirname+'/index.html'));
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
     }
 });
 
